1044c90b9e1ada5a2e2a303bb35f75ea3c6f415f
needleful <needleful@outlook.com>
2026-09-09 20:20:19 -0700

    Utility to reimport files

--- editor_console/console_script.gd
+++ editor_console/console_script.gd
@@ -35,3 +35,34 @@ func _set_owner_recursive(p_node: Node, p_from: Node, p_owner: Node):
 		p_node.owner = p_owner
 	for c in p_node.get_children():
 		_set_owner_recursive(c, p_from, p_owner)
+
+func filesystem() -> EditorFileSystem:
+	return EditorInterface.get_resource_filesystem()
+
+func reimport_all(ext: String):
+	var files := files_by_extension(ext)
+	filesystem().reimport_files(files)
+	return files
+# Based on code by hiulit
+# https://gist.github.com/hiulit/772b8784436898fd7f942750ad99e33e
+func files_by_extension(file_ext: String, root := "res://") -> Array:
+	var dir := DirAccess.open(root)
+
+	if not dir:
+		print("An error occurred when trying to access %s." % root)
+		return []
+
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	var result := []
+	while file_name:
+		if dir.current_is_dir():
+			result.append_array(files_by_extension(file_ext,
+				dir.get_current_dir() + '/'+file_name))
+		else:
+			if file_ext and file_name.get_extension() != file_ext:
+				file_name = dir.get_next()
+				continue
+			result.append(file_name)
+		file_name = dir.get_next()
+	return result
